d35693e6c3f5c3ad9f400664a3f6f91c99c58a93
Jodjo <joseph.mogaizel@gmail.com>
2025-01-09 23:18:57 +0100

    Adding update method, adding id management when deleting tasks and refactoring

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.use(express.json());
 
 let toDo = [];
 
+const writeToFile = (toDos) => {
+	const tasks = JSON.stringify(toDos);
+
+	fs.writeFile("tasks.json", tasks, (err) => {
+		if (err) throw err;
+		console.log("Data added or deleted");
+	});
+};
+
 fs.readFile("tasks.json", (err, jsonData) => {
 	if (err) throw err;
 	data = JSON.parse(jsonData);
@@ -31,7 +40,11 @@ app.post("/todo-list", (req, res) => {
 		return res
 			.status(400)
 			.json({ error: "Status must be at least 3 characters" });
-	} else if (status.toLowerCase() !== "To Do".toLowerCase() && status.toLowerCase() !== "Ongoing".toLowerCase() && status.toLowerCase() !== "Done".toLowerCase()) {
+	} else if (
+		status.toLowerCase() !== "To Do".toLowerCase() &&
+		status.toLowerCase() !== "Ongoing".toLowerCase() &&
+		status.toLowerCase() !== "Done".toLowerCase()
+	) {
 		return res.status(400).json({
 			error: "Status must be To do, or Ongoing or Done",
 		});
@@ -46,24 +59,45 @@ app.post("/todo-list", (req, res) => {
 	toDo.push(newToDo);
 	res.status(201).json(newToDo);
 
-	const tasks = JSON.stringify(toDo);
-
-	fs.writeFile("tasks.json", tasks, (err) => {
-		if (err) throw err;
-		console.log("Data added");
-	});
+	writeToFile(toDo);
 });
 
 app.delete("/todo-list/:id", (req, res) => {
 	const id = Number(req.params.id);
 
 	const itemIndex = toDo.findIndex((toDoTask) => toDoTask.id === id);
-	console.log(itemIndex);
-	console.log(toDo.splice(itemIndex + 1, 1));
 
-	toDo.splice(itemIndex + 1, 1);
-	console.log("This task was deleted:", toDo.splice(itemIndex + 1, 1));
+	if (itemIndex === -1) {
+		return res.status(400).json({
+			error: "Task doesn't exist",
+			description: `There are currently ${toDo.length} tasks`,
+		});
+	}
+
+	toDo.splice(itemIndex, 1);
+
+	for (let i = itemIndex; i < toDo.length; i++) {
+		toDo[i].id = toDo[i].id - 1;
+	}
+
 	res.status(200).send();
+
+	writeToFile(toDo);
+});
+
+app.put("/todo-list/:id", (req, res) => {
+	const id = Number(req.params.id);
+	const title = String(req.body.title);
+	const status = String(req.body.status);
+
+	const item = toDo.find((task) => task.id === id);
+
+	item.title = title || item.title;
+	item.status = status || item.status;
+
+	res.status(200).send();
+
+	writeToFile(toDo);
 });
 
 const port = 3000;
