ac64943b8c563ae6b7e947c175600b990c846357
Jodjo <joseph.mogaizel@gmail.com>
2025-01-21 15:47:17 +0100

    Removing utils

--- tests/tasks.test.js
+++ tests/tasks.test.js
@@ -1,6 +1,5 @@
 const app = require("../app");
 const request = require("supertest");
-const reqAddTask = require("../utils/data/task.data.test.js");
 
 describe("API tasks", () => {
 	it("should return all tasks", async () => {
