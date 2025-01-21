f2e18006475673b3cb74b3d2097024b1f5be9b15
Jodjo <joseph.mogaizel@gmail.com>
2025-01-21 15:46:14 +0100

    Integrating mysql2 to app

--- config/db.js
+++ config/db.js
@@ -1,14 +1,10 @@
-const mysql = require("mysql2/promise");
-require("dotenv").config();
+const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-	waitForConnections: true,
-	connectionLimit: 10,
-	queueLimit: 0,
-  });
+const connection = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "iZu__T}&9z6Nc,&+OT}]",
+	database: "tasks",
+});
 
-  module.exports = pool;
+module.exports = connection;
