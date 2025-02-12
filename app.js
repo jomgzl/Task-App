b8a8e1cd2fa38ec83514f7e80bc5caf0580fc43e
Jodjo <joseph.mogaizel@gmail.com>
2025-02-12 18:28:16 +0100

    Converting index.html to ejs and putting in place a modular navbar

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+const homeRoute = require("./routes/homeRouter");
 const usersRoutes = require("./routes/usersRouter");
 const tasksRoutes = require("./routes/tasksRouter");
 const aboutRoute = require("./routes/aboutRouter");
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(express.static("views"));
 app.use(express.static("public"));
 
+app.use("/", homeRoute);
 app.use("/user", usersRoutes);
 app.use("/api", tasksRoutes);
 app.use("/", aboutRoute);
